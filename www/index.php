c2ed6acddbc6a8f51311ec7384d41516998b5d8d
GREGORY <gde-pass@student.42.fr>
2018-08-15 19:39:33 +0200

    index.php cleared

--- www/index.php
+++ www/index.php
@@ -1,12 +1,4 @@
-<?php include('./config/database.php'); ?>
+<?php include('./header.php'); ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Camagru</title>
-    </head>
-    <body>
-        <?php echo '<p>hola</p>'; ?>
     </body>
-
 </html>
