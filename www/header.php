7cdb30aed5eb6376b713336852e39614e1f23a2e
Gregory DE PASS <gde-pass@e3r8p11.42.fr>
2018-08-16 14:57:43 +0200

    header with his css added templates folder created

--- www/header.php
+++ www/header.php
@@ -3,7 +3,28 @@
 
 <head>
     <title>Camagru</title>
+    <link href="./style/header.css" rel="stylesheet" type="text/css">
     <link href="./style/footer.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
+    <header class="header-login-signup">
+
+    	<div class="header-limiter">
+
+    		<h1><a href="#">Cam<span>agru</span></a></h1>
+
+    		<nav>
+    			<a href="./index.php">Home</a>
+    			<a href="#" class="selected">Blog</a>
+    			<a href="#">Pricing</a>
+    		</nav>
+
+    		<ul>
+    			<li><a href="#">Login</a></li>
+    			<li><a href="./form_subscription.php">Sign up</a></li>
+    		</ul>
+
+    	</div>
+
+    </header>
