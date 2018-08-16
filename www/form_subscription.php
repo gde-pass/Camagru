58ac6f6b98853e7304163734211a059023350254
Gregory DE PASS <gde-pass@e3r8p11.42.fr>
2018-08-16 15:58:31 +0200

    subscribtion form css added

--- www/form_subscription.php
+++ www/form_subscription.php
@@ -1,22 +1,82 @@
 <?php
 include('./header.php');
 ?>
-<form action="./subscription.php" target="_self" method="post">
-    <fieldset>
-        <legend>Subscription:</legend>
-        First name:<br>
-        <input type="text" name="firstname" autofocus><br>
-        Last name:<br>
-        <input type="text" name="lastname"><br>
-        Login:<br>
-        <input type="text" name="login"><br>
-        Email:<br>
-        <input type="email" name="email"><br>
-        Password:<br>
-        <input type="password" name="password"><br><br>
-        <input type="submit" value="Submit">
-    </fieldset>
-</form>
+<div class="main-content">
+
+    <!-- You only need this form and the form-register.css -->
+
+    <form class="form-register" method="post" action="#">
+
+        <div class="form-register-with-email">
+
+            <div class="form-white-background">
+
+                <div class="form-title-row">
+                    <h1>Create an account</h1>
+                </div>
+
+                <div class="form-row">
+                    <label>
+                        <span>First Name</span>
+                        <input type="text" name="firstname">
+                    </label>
+                </div>
+
+                <div class="form-row">
+                    <label>
+                        <span>Last Name</span>
+                        <input type="text" name="lastname">
+                    </label>
+                </div>
+
+                <div class="form-row">
+                    <label>
+                        <span>Email</span>
+                        <input type="email" name="email">
+                    </label>
+                </div>
+
+                <div class="form-row">
+                    <label>
+                        <span>Password</span>
+                        <input type="password" name="password">
+                    </label>
+                </div>
+
+                <div class="form-row">
+                    <label class="form-checkbox">
+                        <input type="checkbox" name="checkbox" checked>
+                        <span>I agree to the <a href="#">terms and conditions</a></span>
+                    </label>
+                </div>
+
+                <div class="form-row">
+                    <button type="submit">Register</button>
+                </div>
+
+            </div>
+
+            <a href="#" class="form-log-in-with-existing">Already have an account? Login here &rarr;</a>
+
+        </div>
+
+        <div class="form-sign-in-with-social">
+
+            <div class="form-row form-title-row">
+                <span class="form-title">Sign in with</span>
+            </div>
+
+            <a href="#" class="form-google-button">Google</a>
+            <a href="#" class="form-facebook-button">Facebook</a>
+            <a href="#" class="form-twitter-button">Twitter</a>
+
+        </div>
+
+    </form>
+
+</div>
+
+
 <?php
 include('./footer.php');
 ?>
--- www/header.php
+++ www/header.php
@@ -4,6 +4,7 @@
 <head>
     <title>Camagru</title>
     <link href="./style/header.css" rel="stylesheet" type="text/css">
+    <link href="./style/form.css" rel="stylesheet" type="text/css">
     <link href="./style/footer.css" rel="stylesheet" type="text/css">
 </head>
 
@@ -12,7 +13,7 @@
 
     	<div class="header-limiter">
 
-    		<h1><a href="#">Cam<span>agru</span></a></h1>
+    		<h1><a href="./index.php">Cam<span>agru</span></a></h1>
 
     		<nav>
     			<a href="./index.php">Home</a>
@@ -21,7 +22,7 @@
     		</nav>
 
     		<ul>
-    			<li><a href="#">Login</a></li>
+    			<li><a href="./form_login.php">Login</a></li>
     			<li><a href="./form_subscription.php">Sign up</a></li>
     		</ul>
 
