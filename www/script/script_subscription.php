34e89fdd6da887b4ba9f25c45f87ed5bbe957e25
GREGORY <gde-pass@student.42.fr>
2018-08-23 22:04:19 +0200

    Pattern check server side done for subform

--- www/script/script_subscription.php
+++ www/script/script_subscription.php
@@ -2,26 +2,27 @@
 
 // print_r($_POST);
 
-function check_flname($firstname, $lastname)
+function server_pattern_check($firstname, $lastname, $nickname, $password,)
 {
-    if(!preg_match("/^[À-ÿa-zA-Z'-]+$/",$firstname))
+    if (!preg_match("/^[À-ÿa-zA-Z'-]+$/",$firstname))
      {
          die ("invalid first name");
      }
-    if(!preg_match("/^[À-ÿa-zA-Z'-]+$/",$lastname))
+
+    if (!preg_match("/^[À-ÿa-zA-Z'-]+$/",$lastname))
      {
          die ("invalid last name");
      }
-}
 
-function check_nickname($nickname)
-{
-    if(!preg_match("/^[0-9a-zA-Z'-]+$/",$nickname))
+    if (!preg_match("/^[0-9a-zA-Z'-]+$/",$nickname))
     {
         die ("invalid nickname");
     }
+
+    if (!preg_match("/((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})/",$password))
+    {
+        die ("invalid password");
+    }
 }
 
-check_flname($_POST["firstname"], $_POST["lastname"]);
-check_nickname($_POST["nickname"]);
 ?>
