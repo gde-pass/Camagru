bc9c1633c207237692ed04dc253efa4ad77a2fc2
GREGORY <gde-pass@student.42.fr>
2018-08-23 20:41:48 +0200

    Nothing changed ...

--- www/script/script_subscription.php
+++ www/script/script_subscription.php
@@ -8,7 +8,7 @@ function check_flname($firstname, $lastname)
      {
          die ("invalid first name");
      }
-    if(!preg_match("/^[À-ÿa-zA-Z'-]+$/",$firstname))
+    if(!preg_match("/^[À-ÿa-zA-Z'-]+$/",$lastname))
      {
          die ("invalid last name");
      }
@@ -16,7 +16,10 @@ function check_flname($firstname, $lastname)
 
 function check_nickname($nickname)
 {
-
+    if(!preg_match("/^[0-9a-zA-Z'-]+$/",$nickname))
+    {
+        die ("invalid nickname");
+    }
 }
 
 check_flname($_POST["firstname"], $_POST["lastname"]);
