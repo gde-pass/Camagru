145ac1b2044cc3306975f37911e24799c0a6ccec
GREGORY <gde-pass@student.42.fr>
2018-09-02 17:22:20 +0200

    Mail sended still not working

--- www/script/mail_sended.php
+++ www/script/mail_sended.php
@@ -1,24 +1,42 @@
 <?php
-// sleep(5);
+sleep(5);
 
-$to = $_GET['email'];
-$subject = "c pour confirm ton mail\n";
-$message = "la yora un lien klikable\n";
-$headers = 'From: webmaster@example.com' . "\r\n" .
-     'Reply-To: webmaster@example.com' . "\r\n" .
-     'X-Mailer: PHP/' . phpversion();
+$from = '<project.camagru.42@gmail.com>';
+$to = '<' . $_GET['email'] . '>';
+$subject = 'Hi!';
+$body = "Hi,\n\nPlease confirm your mail by following this link: LINK";
+
+$headers = array
+(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'project.camagru.42@gmail.com',
+        'password' => 'Camagru42'
+    ));
 
 #ENVOIE DE MAIL
+$mail = $smtp->send($to, $headers, $body);
 
-mail ($to, $subject, $message, $headers)
 #AFFICHAGE
+if (PEAR::isError($mail))
+{
+    echo('<p>' . $mail->getMessage() . '</p>');
+}
+else
+{
+    echo('<p>Message successfully sent!</p>');
+}
 
-
-
-
-
-// echo "
-//     <script language='JavaScript' type='text/javascript'>
-//         window.location.replace('../form/form_login.php');
-//     </script>";
+# Redirection to login page
+echo "
+    <script language='JavaScript' type='text/javascript'>
+        window.location.replace('../form/form_login.php');
+    </script>";
 ?>
