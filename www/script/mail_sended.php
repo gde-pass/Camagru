382e85378fd0627e4b259e799771f8c4956c0605
GREGORY <gde-pass@student.42.fr>
2018-09-10 23:19:50 +0200

    restart from nothing about mailling

--- www/script/mail_sended.php
+++ www/script/mail_sended.php
@@ -1,38 +1,12 @@
 <?php
-sleep(5);
+sleep(3);
 
-$from = '<project.camagru.42@gmail.com>';
-$to = '<' . $_GET['email'] . '>';
+$to = $_GET['email'];
 $subject = 'Hi!';
-$body = "Hi,\n\nPlease confirm your mail by following this link: LINK";
+$message = "Hi,\n\nPlease confirm your mail by following this link: LINK";
 
-$headers = array
-(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject
-);
 
-$smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'project.camagru.42@gmail.com',
-        'password' => 'Camagru42'
-    ));
-
-#ENVOIE DE MAIL
-$mail = $smtp->send($to, $headers, $body);
-
-#AFFICHAGE
-if (PEAR::isError($mail))
-{
-    echo('<p>' . $mail->getMessage() . '</p>');
-}
-else
-{
-    echo('<p>Message successfully sent!</p>');
-}
+mail($to, $subject, $message);
 
 # Redirection to login page
 echo "
