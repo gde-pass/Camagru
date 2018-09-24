0411c7066a40f96ab62b25a930d0b527317bda96
Gregory DE PASS <gde-pass@e1r12p2.42.fr>
2018-09-24 17:33:37 +0200

    script sub in progress

--- www/script/script_subscription.php
+++ www/script/script_subscription.php
@@ -1,8 +1,8 @@
 <?php
 include '../config/database.php';
+include '../header.php';
 
 # Check patterns server side
-
 function server_pattern_check($firstname, $lastname, $nickname, $password, $email)
 {
     if (!preg_match("/^[À-ÿa-zA-Z' -]+$/",$firstname))
@@ -28,7 +28,6 @@ function server_pattern_check($firstname, $lastname, $nickname, $password, $emai
         die ("invalid password");
         return FALSE;
     }
-    return TRUE;
 
     if (!preg_match("/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/",$email))
     {
@@ -36,64 +35,80 @@ function server_pattern_check($firstname, $lastname, $nickname, $password, $emai
         return FALSE;
     }
     return TRUE;
-
 }
 
 if (isset($_POST["firstname"], $_POST["lastname"], $_POST["nickname"], $_POST["email"], $_POST["password"], $_POST["termsandconditions"]))
 {
+    #convert everthing into local varibles
     $firstname = $_POST["firstname"];
     $lastname = $_POST["lastname"];
     $nickname = $_POST["nickname"];
     $email = $_POST["email"];
     $password = $_POST["password"];
-
     # generating random key
     $key = md5(microtime(TRUE)*100000);
 
-    if (server_pattern_check($firstname, $lastname, $nickname, $password, $email) === TRUE);
+    if (server_pattern_check($firstname, $lastname, $nickname, $password, $email) === TRUE)
     {
-        try
-        {
-            #Hash the password
-            $password = hash('whirlpool', $password);
             #Connection to DB camagru
             $dbh = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
             #set the PDO error mode to exception
             $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            #set the sql request for insert the new user
-            $sql = "INSERT INTO `users` (`id`, `nickname`, `password`, `email`, `firstname`, `lastname`, `key`)
-                    VALUES (NULL, '$nickname', '$password', '$email', '$firstname', '$lastname', '$key')";
-            #run the sql request
-            $dbh->exec($sql);
 
-            # Setting up mail.txt in /tmp
-$mail =
-'From: Camagru <camagru@horsefucker.org>
-To: '.$firstname.' '.$lastname.' <'.$email.'>
-Subject: Email confirmation
+            $sql = $dbh->prepare("SELECT * FROM `users` WHERE `users`.`nickname` = ? AND `users`.`email` = ?");
+            $sql->execute([$nickname, $email]);
+            $user_availability = $sql->fetch(PDO::FETCH_ASSOC);
 
-To confirm your registration please click or copy and paste the following link into your web browser
-http://192.168.99.100/script/mail_confirmation.php?nickname='.urlencode($nickname).'&key='.urlencode($key).'
+            echo "<pre>".count($user_availability)."</pre>";
+            echo "<pre>".$user_availability['nickname']."</pre>";
+            if (empty($user_availability))
+            {
+                echo "JJANIEC";
+            }
+            var_dump($user_availability);
+/*            $count = $dbh->exec($sql);
+            if ($count === 0)
+            {
+                #Hash the password
+                $password = hash('whirlpool', $password);
+                #set the sql request for insert the new user
+                $sql = "INSERT INTO `users` (`id`, `nickname`, `password`, `email`, `firstname`, `lastname`, `key`)
+                        VALUES (NULL, '$nickname', '$password', '$email', '$firstname', '$lastname', '$key')";
+                #run the sql request
+                $ret = $dbh->exec($sql);
 
-This is an automatically generated email, please do not reply to it.
-If you have any queries regarding your order please email gde-pass@student.42.fr';
-
-             # Send the mail and deleting tmp file
-
-             shell_exec('echo "'.$mail.'" > /tmp/mail.txt');
-             shell_exec('sh mail.sh "'.$email.'" > /tmp/log.txt 2>&1');
-             shell_exec('rm -rf /tmp/mail.txt');
-
-             # Redirection to login page
-             echo "
-                 <script language='JavaScript' type='text/javascript'>
-                     window.location.replace('../form/form_login.php');
-                 </script>";
-        }
-        catch(PDOException $e)
-        {
-            echo $sql . "\n" . $e->getMessage();
-        }
+                $ret->fetchAll(PDO::FETCH_ASSOC);
+            }*/
+                # Setting up mail.txt in /tmp
+//                 $mail =
+// 'From: Camagru <camagru@horsefucker.org>
+// To: '.$firstname.' '.$lastname.' <'.$email.'>
+// Subject: Email confirmation
+//
+// To confirm your registration please click or copy and paste the following link into your web browser
+// http://192.168.99.100/script/mail_confirmation.php?nickname='.urlencode($nickname).'&key='.urlencode($key).'
+//
+// This is an automatically generated email, please do not reply to it.
+// If you have any queries regarding your order please email gde-pass@student.42.fr';
+//
+//              # Send the mail and deleting tmp file
+//
+//              shell_exec('echo "'.$mail.'" > /tmp/mail.txt');
+//              shell_exec('sh mail.sh "'.$email.'" > /tmp/log.txt 2>&1');
+//              shell_exec('rm -rf /tmp/mail.txt');
+//
+//              # Redirection to login page
+//              echo "
+//                  <script language='JavaScript' type='text/javascript'>
+//                      window.location.replace('../form/form_login.php?email=sent');
+//                  </script>";
+//           }
+             // else
+             // {
+             //     echo "EMAIL OU NICKNAME DEJA USE";
+             // }
     }
 }
+
+include '../footer.php';
 ?>
