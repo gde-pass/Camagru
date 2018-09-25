42c2118dc486ef8cfebe10829e5c21389e27cf34
Gregory DE PASS <gde-pass@e1r12p4.42.fr>
2018-09-25 17:09:26 +0200

    mail.sh deleted

--- www/script/mail.sh
+++
@@ -1,8 +0,0 @@
-curl -v \
-        --url 'smtps://mail.cock.li:465' \
-        --ssl-reqd \
-        --mail-from 'camagru@horsefucker.org' \
-        --mail-rcpt $1 \
-        --upload-file /tmp/mail.txt \
-        --user 'camagru@horsefucker.org:Camagru42' \
-        --insecure -v
