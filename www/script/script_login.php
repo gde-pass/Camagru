cce838bd62188a50a10b2f85d1ce2e7362ec23f7
Gregory DE PASS <gde-pass@e2r12p4.42.fr>
2018-09-29 13:00:52 +0200

    seting up session varibles when login

--- www/script/script_login.php
+++ www/script/script_login.php
@@ -1,29 +1,72 @@
 <?php
+include '../config/database.php';
+session_start();
 
-static function server_pattern_check($email, $password)
+function server_pattern_check($password, $email)
 {
-    if (!preg_match("/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/",$email))
-     {
-         die ("invalid email");
-         return FALSE;
-     }
-
     if (!preg_match("/((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})/",$password))
     {
         die ("invalid password");
         return FALSE;
     }
+
+    if (!preg_match("/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/",$email))
+    {
+        die ("invalid email");
+        return FALSE;
+    }
     return TRUE;
 }
 
-if (isset($_POST["email"], $_POST["password"])
+if (isset($_POST["email"], $_POST["password"]) AND !empty($_POST["email"]) AND !empty($_POST["password"]) AND server_pattern_check($_POST["password"], $_POST["email"]) === TRUE)
 {
-
-if TRUE -> server_pattern_check($email, $password)
-
-# REQUETE SQL POUR VERIFIER SI LE COMPTE EXISTE
-
-
+    #convert everthing into local varibles
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    #Hash the password
+    $password = hash('whirlpool', $password);
+    #Connection to DB camagru
+    $dbh = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
+    #set the PDO error mode to exception
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = $dbh->prepare("SELECT * FROM `users` WHERE `users`.`email` = ? AND `users`.`password` = ?");
+    $sql->execute([$email, $password]);
+    $user_validity = $sql->fetch(PDO::FETCH_ASSOC);
+    if ($user_validity !== FALSE)
+    {
+        #if this code has been executed that mean the user type the good combination of PW and Email
+        if ($user_validity['confirm'] == 1)
+        {
+            #setup users informtions in session variables
+            $_SESSION['logged'] = TRUE;
+            $_SESSION['firstname'] = $user_validity['firstname'];
+            $_SESSION['lastname'] = $user_validity['lastname'];
+            $_SESSION['nickname'] = $user_validity['nickname'];
+            $_SESSION['email'] = $user_validity['email'];
+            #redirection to the index page
+            echo "
+                <script language='JavaScript' type='text/javascript'>
+                    window.location.replace('../index.php');
+                </script>";
+        }
+        else
+        {
+            # If this following code has been executed that mean the user didn't confirm his account
+            # Redirection to login page
+             echo "
+                 <script language='JavaScript' type='text/javascript'>
+                     window.location.replace('../form/form_login.php?confirm=no');
+                 </script>";
+        }
+    }
+    else
+    {
+        # Redirection to login page
+         echo "
+             <script language='JavaScript' type='text/javascript'>
+                 window.location.replace('../form/form_login.php?log=no');
+             </script>";
+    }
 }
 
 
