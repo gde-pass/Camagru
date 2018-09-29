5d9627d5c0a81ebd535fbc4991f0dbfb95ba1085
Gregory DE PASS <gde-pass@e2r12p4.42.fr>
2018-09-29 11:12:29 +0200

    redirection added

--- www/script/script_change_password.php
+++ www/script/script_change_password.php
@@ -28,6 +28,11 @@ if (isset($_POST['password']) AND !empty($_POST['password']) AND server_pattern_
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = $dbh->prepare("UPDATE `users` SET `password` = ?, `reset_token` = NULL, `date_token` = NULL WHERE `users`.`email` = ? AND `users`.`reset_token` = ?");
     $sql->execute([$password, $email, $token]);
+    # redirection
+    echo "
+     <script language='JavaScript' type='text/javascript'>
+         window.location.replace('../form/form_login.php?password=changed');
+     </script>";
 }
 
 ?>
