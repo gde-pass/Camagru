f86d1f95af9cd67ac9071983861da597c22a9148
Gregory DE PASS <gde-pass@e3r5p20.42.fr>
2018-10-03 11:38:42 +0200

    script update avatar

--- www/script/setting.php
+++ www/script/setting.php
@@ -1,5 +1,24 @@
 <?php
-if ($_POST['avatar'])
+session_start();
+include '../config/database.php';
+
+if (isset($_POST['avatar']) AND !empty($_POST['avatar']))
+{
+    #convert into local variable
+    $avatar = $_POST['avatar'];
+    #Connection to DB camagru
+    $dbh = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
+    #set the PDO error mode to exception
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = $dbh->prepare("UPDATE `users` SET `avatar` = ?");
+    $sql->execute([$avatar]);
+    http_response_code(200);
+    return ($avatar);
+}
+else
+{
+    http_response_code(500);
+}
 
 
 ?>
