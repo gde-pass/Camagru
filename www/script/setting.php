42bfb571c10185deda5e60662fc545469ab98156
Gregory DE PASS <gde-pass@e3r5p20.42.fr>
2018-10-03 13:27:09 +0200

    fix quesry update avatar

--- www/script/setting.php
+++ www/script/setting.php
@@ -6,15 +6,16 @@ if (isset($_POST['avatar']) AND !empty($_POST['avatar']))
 {
     #convert into local variable
     $avatar = $_POST['avatar'];
+    $email = $_SESSION['email'];
+    $nickname = $_SESSION['nickname'];
     #Connection to DB camagru
     $dbh = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
     #set the PDO error mode to exception
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = $dbh->prepare("UPDATE `users` SET `avatar` = ?");
-    $sql->execute([$avatar]);
+    $sql = $dbh->prepare("UPDATE `users` SET `avatar` = ? WHERE `users`.`email` = ? AND `users`.`nickname` = ?");
+    $sql->execute([$avatar, $email, $nickname]);
     $_SESSION['avatar'] = $avatar;
     http_response_code(200);
-    return ($avatar);
 }
 else
 {
