85324f40da82c4f0a417d65d0c7b9bf13b10dccc
Gregory DE PASS <gde-pass@e3r6p18.42.fr>
2018-10-05 14:06:25 +0200

    delete account and the dir of the user to

--- www/script/script_delaccount.php
+++ www/script/script_delaccount.php
@@ -2,6 +2,15 @@
 session_start();
 include '../config/database.php';
 
+function removeDirectory($path) {
+ 	$files = glob($path . '/*');
+	foreach ($files as $file) {
+		is_dir($file) ? removeDirectory($file) : unlink($file);
+	}
+	rmdir($path);
+ 	return;
+}
+
 if (isset($_SESSION) AND !empty($_SESSION))
 {
     $dbh = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
@@ -9,6 +18,9 @@ if (isset($_SESSION) AND !empty($_SESSION))
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = $dbh->prepare("DELETE FROM `users` WHERE `users`.`email` = ? AND `users`.`nickname` = ?");
     $sql->execute([$_SESSION['email'], $_SESSION['nickname']]);
+
+    #delete pictures of the users
+    removeDirectory("../data/" . $_SESSION['nickname']);
     # redirection
     header("location:logout.php");
 }
