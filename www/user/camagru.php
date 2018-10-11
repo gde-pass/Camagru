20ccecbae6d683599d23cc2039acbecb99221b3a
Jean-baptiste BLONDEAU <jeblonde@e3r5p18.42.fr>
2018-10-11 16:07:51 +0200

    filtres++

--- www/user/camagru.php
+++ www/user/camagru.php
@@ -27,24 +27,29 @@ include '../header.php';
               <canvas id="canvas2" onclick="selected(2, this)" class="camagru-canvas" style="visibility:hidden"></canvas>
             </label>
         </div>
-        <div class="container-filtres">
-          <input type="radio" name="filtres" id="filtre0" hidden>
+        <form class="container-filtres">
+          <input type="radio" name="filtres" id="filtre0" value="0" onclick="applyFiltre(this)" hidden>
           <label for="filtre0">
+            <img src="/img/filtre/empty.png" alt="" title="">
+          </label>
+          <input type="radio" name="filtres" id="filtre1" value="1" onclick="applyFiltre(this)" hidden>
+          <label for="filtre1">
             <img src="/img/filtre/star.png" alt="" title="">
           </label>
-          <input type="radio" name="filtres" id="filtre1" hidden>
-          <label for="filtre1">
+          <input type="radio" name="filtres" id="filtre2" value="2" onclick="applyFiltre(this)" hidden>
+          <label for="filtre2">
             <img src="/img/filtre/circular.png" alt="" title="">
           </label>
-          <input type="radio" name="filtres" id="filtre2" hidden>
-          <label for="filtre2">
+          <input type="radio" name="filtres" id="filtre3" value="3" onclick="applyFiltre(this)" hidden>
+          <label for="filtre3">
             <img src="/img/filtre/smoke.png" alt="" title="">
           </label>
-          <input type="radio" name="filtres" id="filtre3" hidden>
-          <label for="filtre3">
+          <input type="radio" name="filtres" id="filtre4" value="4" onclick="applyFiltre(this)" hidden>
+          <label for="filtre4">
             <img src="/img/filtre/flower.png" alt="" title="">
           </label>
-        </div>
+        </form>
+        <textarea name="comment"></textarea>
         <button onClick="send()">Send</button>
 
   <script>
@@ -54,26 +59,27 @@ include '../header.php';
   const video = document.getElementById("video-camera");
   var canvas = document.getElementById("canvas" + nb);
   const context = canvas.getContext('2d');
-  const f = ["", "", ""];
+  const f = ["0", "0", "0"];
   var w = 640;
   var h = 480;
+  const filtreMax = 4;
+
 
   function selected(i, element) {
     if (i < nb)
       selection = i;
+    else
+      return ;
     console.log(selection);
+    document.getElementById("filtre" + f[i]).checked = true;
   }
 
-
-
-
-
-
-
-
-
-
-
+  function applyFiltre(element) {
+    console.log(element.value);
+    if (nb === 0)
+      return ;
+    f[selection] = element.value;
+  }
 
   function send() {
     let i = 0;
@@ -122,7 +128,7 @@ include '../header.php';
       context.fillRect(0, 0, w, h);
       context.drawImage(video, 0, 0, w, h);
       context.globalAlpha = 0.5;
-      img = document.getElementById("f0");
+      let img = document.getElementById("f1");
       context.drawImage = (img, 0, 0, w, h);
       canvas.style.visibility = "visible";
       nb++;
